784234bfe5134682a93462b5e3581512710be4b0
Donia Alhosin <doniaelhussien@gmail.com>
2024-10-22 10:09:25 +0300

    Deploy Vercel - sql via railway

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -312,7 +312,7 @@ return [
          * Constants which can be used in annotations
          */
         'constants' => [
-            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
+            'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://restaurant-server-pi.vercel.app'),
         ],
     ],
 ];
